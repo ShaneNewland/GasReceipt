70f671ea14d4ad20e8e049fdb4f4a7dae5903e3c
Shane <shane.newland.2023@student.lhschools.org>
2020-02-25 13:18:00 -0500

    Intellij Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,29 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+----------------------------+");
+        System.out.println("| Shane's Gas Station        |");
+        System.out.println("| 13075 Worthington Rd       |");
+        System.out.println("| 7032 Broad St SW           |");
+        System.out.println("|   Thank You For Using      |");
+        System.out.println("| Amount      Fuel Total     |");
+        System.out.println("|   Shane's Gas Station!     |");
+        System.out.println("|   Date          Time       |");
+        System.out.println("| 14JAN2020      1:12 PM     |");
+        System.out.println("| Invoice Number#  983540    |");
+        System.out.println("| Pump   Product      $/G    |");
+        System.out.println("| Auth#                      |");
+        System.out.println("| MasterCard                 |");
+        System.out.println("| **************09           |");
+        System.out.println("| 06    unleaded     $2.69   |");
+        System.out.println("| 20.00       $53.80         |");
+        System.out.println("|                            |");
+        System.out.println("|   Thank You For Using      |");
+        System.out.println("|   Shane's Gas Station!     |");
+        System.out.println("+----------------------------+");
+
+    }
+
+}
